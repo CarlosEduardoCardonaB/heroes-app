e4354abda48247af0e031985482aeb6583f879d1
Carlos Eduardo <caeca425@gmail.com>
2024-10-15 21:06:15 -0500

    se avanza en la creación de los métodos crud y la implemntación del reactive form para el ingreso del super héroe

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Hero } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-page',
@@ -11,10 +12,37 @@ export class NewPageComponent {
 
   constructor( private heroService: HeroesService ){}
 
+  public heroForm = new FormGroup({
+      id:               new FormControl<String>(''),
+      superhero:        new FormControl<String>('', { nonNullable: true }),
+      publisher:        new FormControl<Publisher>(Publisher.DCComics),
+      alter_ego:        new FormControl(''),
+      first_appearance: new FormControl(''),
+      characters:       new FormControl(''),
+      alt_img:          new FormControl('')
+  })
+
   public creatorList = [
     {id:'DC Comics', desc: 'DC -  Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'}
-  ]
+  ];
+
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmitForm():void{
+    console.log({
+      fomIsValid: this.heroForm.valid,
+      vale: this.heroForm.value,
+    });
+
+    if(this.heroForm.invalid ) return;
+
+    //this.heroService.addHero( this.heroForm.value )
+
+  }
 
   addHero( hero: Hero ){
     this.heroService.addHero(hero);
